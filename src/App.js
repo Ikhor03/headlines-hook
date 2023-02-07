3954cde96a3faecbb4a421e061acd7f82d8d9a59
Ikhor03 <imamkhoiri725@gmail.com>
2023-02-07 22:32:56 +0700

    sincronize cards with API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import LiveSearch from './livesearch';
 import Cards from './cards';
-import { Container } from 'react-bootstrap';
 
 
 function App() {
   return (
     <div className='bg-dark'>
+
       <nav className="navbar bg-warning">
         <div className="container">
           <a className="navbar-brand fw-bold text-dark m-auto" href="https://">News Site</a>
@@ -14,22 +15,8 @@ function App() {
       </nav>
 
       <div className="container-fluid">
-        <nav className="navbar mt-3">
-          <form className="container-fluid">
-            <div className="input-group">
-              <input id="searchInput" type="text" className="form-control" placeholder="Find some insight.."
-                aria-label="Username" aria-describedby="basic-addon1" />
-            </div>
-          </form>
-        </nav>
-        <p className="headline-badge mt-2 fw-bold text-white offset-1 fs-4"><span className="badge bg-warning">Headlines</span>
-          today</p>
-      </div>
-
-
-      <Container fluid>
         <Cards />
-      </Container>
+      </div>
 
     </div>
 
--- src/cards.js
+++ src/cards.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Form, Row } from "react-bootstrap";
 // import NotFound, { Loading } from "./alert";
 
 
@@ -7,39 +7,82 @@ class Cards extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            url: 'https://newsapi.org/v2/top-headlines?country=id&apiKey=1ec087e86b5143d28480549839fbe11c',
-            data: {}
+            data: [],
+            value: '',
+            hide: 'hide',
+            url: 'https://newsapi.org/v2/top-headlines?country=id&apiKey=1ec087e86b5143d28480549839fbe11c'
         }
+
+        this.inputHandler = this.inputHandler.bind(this);
+        // this.componentDidMount = this.componentDidMount.bind(this);
     }
 
-    componentDidMount() {
-        fetch(this.state.url)
+    inputHandler(e) {
+        this.setState({
+            value: e.target.value
+        }, (() => {
+            const { value } = this.state;
+            if (!value) {
+                this.setState({
+                    url: 'https://newsapi.org/v2/top-headlines?country=id&apiKey=1ec087e86b5143d28480549839fbe11c'
+                })
+                console.log('kosong');
+            } else {
+                this.setState({
+                    url: 'https://newsapi.org/v2/everything?q=tesla&from=2023-01-07&sortBy=publishedAt&apiKey=1ec087e86b5143d28480549839fbe11c'
+                })
+                console.log(value);
+            }
+
+        }))
+
+
+    }
+
+    async componentDidMount() {
+
+        await fetch(this.state.url)
             .then(res => res.json())
             .then((data) => {
                 let article = data.articles;
-                // article.map(e => console.log(e))
                 this.setState({
                     data: article
                 })
             })
     }
 
+    //NOTES: COBA PAKAI KONSEP WILL UNMOUNT UNTUK LIVESEARCH
 
     render() {
-        console.log(this.state.data); // NOTE: Sudah dapat datanya, tinggal di akses dan dimasukkan ke DOM
+        const { data } = this.state;
+        let printCard = (data.map((artikel, key) => {
+            return (
+                <Col key={key} xs lg="5" className={`mb-3` }>
+                    <Card style={{ width: '100%' }}>
+                        <Card.Img variant="top" src={artikel.urlToImage} />
+                        <Card.Body>
+                            <Card.Title className="news-card-title card-title h6 fw-semibold">{artikel.title}</Card.Title>
+                            <span className="card-text"><small className="news-card-author text-muted mb-3">{artikel.author}</small></span> <br />
+                            <span className="card-text"><small className="news-card-date text-muted mb-3">{artikel.publishedAt}</small></span>
+                            <Card.Text className="news-card-text card-text text-truncate">{artikel.description}</Card.Text>
+                            <a className="news-url-link btn btn-warning" href={artikel.url} target="_blank" rel='noreferrer'>Read more..</a>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            )
+        }))
+
         return (
-            <Col>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title className="news-card-title card-title h6 fw-semibold">title</Card.Title>
-                        <span className="card-text"><small className="news-card-author text-muted mb-3">tanggal</small></span>
-                        <span className="card-text"><small className="news-card-date text-muted mb-3">tanggal</small></span>
-                        <Card.Text className="news-card-text card-text text-truncate">deskripsi</Card.Text>
-                        <a className="news-url-link btn btn-warning" href='https://' target="_blank" rel='noreferrer'>Read more..</a>
-                    </Card.Body>
-                </Card>
-            </Col>
+            <div>
+                <Form>
+                    <Form.Control onInput={this.inputHandler} value={this.state.value} type="text" placeholder="Find some insight..." className="mt-3" />
+                    <p className="headline-badge mt-2 fw-bold text-white offset-1 fs-4"><span className="badge bg-warning">Headlines</span>
+                        today</p>
+                </Form>
+                <Row className="justify-content-md-center">
+                    {printCard}
+                </Row>
+            </div>
         )
     }
 }
